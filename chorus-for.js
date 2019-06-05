442bb2e4adb313e3988eafd1735798e33b972430
robinwebber <robinewebber@gmail.com>
2019-06-05 20:39:27 +0000

    Use a for loop to repeat a chorus

---
+++ chorus-for.js
@@ -0,0 +1,11 @@
+// Lighthouse labs module 4 for loops
+
+const chorus = 'Move your body!';
+for (let repeat = 0; repeat < 10; repeat ++) {
+  if (repeat % 2 === 0) {
+    console.log('C\'mon!');
+  } else {
+  console.log(chorus);
+  }
+}
+console.log('Across the dance floor!');
