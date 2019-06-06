4592106bacf22d578b77273d724266dae7a6f8f2
robinwebber <robinewebber@gmail.com>
2019-06-06 16:26:00 +0000

    say hello exercise for lighthouse

---
+++ hello.js
@@ -0,0 +1,10 @@
+// Declaring and Calling Functions Lighthouse labs module 4
+
+const sayHello = function (name) {
+  console.log('Hello, ' + name);
+};
+
+sayHello('Andrea');
+sayHello('Robin');
+sayHello('Jamie');
+
