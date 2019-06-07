90efe2b3b41025a2765729fba13b283b7b6e7a9b
robinwebber <robinewebber@gmail.com>
2019-06-07 17:38:50 +0000

    packinglist update

--- packing-list.js
+++ packing-list.js
@@ -6,3 +6,12 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+console.log('\n');
+
+console.log('Kitchen stuff to pack:');
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
